7d1a6317d3d44a1c4e73f35e3596e6ae2b94283c
ArzuAmrah <arzu.amrahova@bk.ru>
2024-06-05 17:47:00 +0400

    initial commit

--- src/lesson/Practice8.java
+++ src/lesson/Practice8.java
@@ -2,8 +2,27 @@ package lesson;
 
 public class Practice8 {
     public static void main(String[] args) {
-        for (int i = 7; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
+
+//        for (int i = 7; i > 0; i--) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        for  (int i = 0; i < 10; i++){
+//            for (int j = 1; j < 10; j++){
+//                System.out.println( " * ");
+//            }
+//        }
+
+
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
                 System.out.print("*");
             }
             System.out.println();
